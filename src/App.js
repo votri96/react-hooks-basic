ee6381c1d08c7bfd0405841382fc450f69d00095
votri96 <votri96@gmail.com>
2023-10-27 00:59:06 +0700

    Add clock

--- src/App.js
+++ src/App.js
@@ -1,62 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import queryString from 'query-string';
 import './App.css';
-import PostList from './components/PostList';
-import Pagination from './components/Pagination';
-import PostFiltersForm from './components/PostFiltersForm';
+import Clock from './components/Clock';
 
 function App() {
-  const [postList, setPostList] = useState([]);
-  const [pagination, setPagination] = useState({});
-  const [filters, setFilters] = useState({
-    _limit: 10,
-    _page: 1,
-  });
-
-  useEffect(() => {
-    async function fetchPostList() {
-      // ...
-      try {
-        const paramsString = queryString.stringify(filters);
-        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
-        const response = await fetch(requestUrl);
-        const responseJSON = await response.json();
-        console.log({ responseJSON });
-
-        const { data, pagination } = responseJSON;
-        setPostList(data);
-        setPagination(pagination);
-      } catch (error) {
-        console.log('Failed to fetch post list: ', error.message);
-      }
-    }
-
-    console.log('POST list effect');
-    fetchPostList();
-  }, [filters]);
-
-  function handlePageChange(newPage) {
-    console.log('New page: ', newPage);
-    setFilters({
-      ...filters,
-      _page: newPage,
-    });
-  }
-
-  function handleFiltersChange(newFilters) {
-    console.log('New filters: ', newFilters);
-    setFilters({
-      ...filters,
-      _page: 1,
-      title_like: newFilters.searchTerm,
-    });
-  }
+  const [showClock, setShowClock] = useState(true);
 
   return (
     <div className="app">
-      <PostFiltersForm onSubmit={handleFiltersChange} />
-      <PostList posts={postList} />
-      <Pagination pagination={pagination} onPageChange={handlePageChange} />
+      {showClock && <Clock />}
+      <button onClick={() => setShowClock(true)}>Show clock</button>
+      <button onClick={() => setShowClock(false)}>Hide clock</button>
     </div>
   );
 }
