9963234d90d8108ac30f2e533ebdaddd5d8985db
votri96 <votri96@gmail.com>
2023-10-28 00:14:11 +0700

    Add useMagicColor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Clock from './components/Clock';
+import MagicBox from './components/MagicBox';
 
 function App() {
   const [showClock, setShowClock] = useState(true);
 
   return (
     <div className="app">
-      {showClock && <Clock />}
-      <button onClick={() => setShowClock(true)}>Show clock</button>
-      <button onClick={() => setShowClock(false)}>Hide clock</button>
+      <MagicBox />
     </div>
   );
 }
